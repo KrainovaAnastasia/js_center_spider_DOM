0385efe3604da7f4d2b1d107764139134465a129
Krainova Anastasia <nastiakray01@gmail.com>
2024-09-18 11:46:00 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+if (spider && wall) {
+    const wallWidth = wall.clientWidth;
+    const wallHeight = wall.clientHeight;
+    const spiderWidth = spider.clientWidth;
+    const spiderHeight = spider.clientHeight;
+    const topPosition = (wallHeight - spiderHeight) / 2;
+    const leftPosition = (wallWidth - spiderWidth) / 2;
+    spider.style.top = `${topPosition}px`;
+    spider.style.left = `${leftPosition}px`;
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
